d47d84954e5d98ec6668c62d222b3a073e207d80
rakeshranjandas <rakeshranjandas.bbsr93@gmail.com>
2023-04-18 09:41:43 +0530

    Style changes in Navbar, ChatList component

--- src/components/mainapp/MainApp.jsx
+++ src/components/mainapp/MainApp.jsx
@@ -1,10 +1,12 @@
 import React from "react"
+import ChatList from "./chat/ChatList"
 import Navbar from "./navbar/Navbar"
 
 export default function MainApp() {
   return (
     <div className="container bg-success">
       <Navbar />
+      <ChatList />
     </div>
   )
 }
